b774a4c2aa6e0e7a1b64382e77ba4a9f50fb3b05
sfaat husain <sfaathusainpintu@gmail.com>
2024-05-11 20:46:51 +0530

    second

--- vehicle/admin.py
+++ vehicle/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+@admin.register( Customer)
+
+class AdminProduct(admin.ModelAdmin):
+
+    list_display=['id','profile_pic']
+
+
+
+
+@admin.register( Mechanic)
+
+class AdminProduct(admin.ModelAdmin):
+
+    list_display=['id','profile_pic']
